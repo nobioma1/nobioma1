de21887ff9d4a8f82ede5232e600c7409b26afd9
Noble Obioma <nobioma1@gmail.com>
2019-10-06 11:08:45 +0100

    Add data obj and avatar

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export default {
+  bio: `Hello, I'm a Full-Stack Web Developer based in Nigeria. 
+    Loving every bit of what I do, creating solutions to real problems that
+    can be solved using technology and also improving already existing solutions.`,
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'GraphQL',
+    'Python',
+    'ReactJs',
+    'Node.js',
+    'Express',
+  ],
+  projects: [
+    {
+      name: 'Luncher',
+      imag: 'luncher',
+      tech: ['React'],
+      github: 'https://github.com/luncher-lambda-buildweek/FrontEnd',
+      link: 'https://luncher-donate-meals.netlify.com',
+    },
+    {
+      name: 'Essentialism',
+      imag: 'esse',
+      tech: ['Javascript'],
+      github:
+        'https://github.com/lambda-essentialism/build-essentialism-WebUI-Noble',
+      link: 'https://esse.netlify.com',
+    },
+    {
+      name: 'Politico',
+      imag: 'politico',
+      tech: ['Javascript', 'NodeJs'],
+      github: 'https://github.com/nobioma1/Politico',
+      link: 'https://nobioma1.github.io/Politico',
+    },
+  ],
+};
