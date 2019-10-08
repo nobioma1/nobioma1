4af9ff360700166dc17cbe1321e29b0e851a59f9
Noble Obioma <nobioma1@gmail.com>
2019-10-08 10:59:49 +0100

    import animate.css

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'animate.css/animate.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
