2571a67e351565a3bb0128259584cfc5d5d2e283
Noble Obioma <nobioma1@gmail.com>
2020-07-06 10:19:11 +0100

    Ignore dependecy array warning

--- src/sections/Articles.jsx
+++ src/sections/Articles.jsx
@@ -47,6 +47,7 @@ const Articles = () => {
     doRequest({
       onSuccess: (res) => setArticles(res),
     });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const articlesList = articles.slice(0, 5).map((article, index) => {
