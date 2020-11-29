1ca9e48f896297f942b966f192f5739a57db2459
Noble Obioma <nobioma1@gmail.com>
2020-11-29 07:30:34 +0100

    Add dependencies - Add typeface Fresca - Add gatsby image dependencies - Add gatsby-alias and create import alias

--- force-awakens/gatsby-config.js
+++ force-awakens/gatsby-config.js
@@ -1,10 +1,26 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-alias-imports`,
+      options: {
+        aliases: {
+          "@components": "src/components",
+          "@layouts": "src/components/layouts",
+          "@sections": "src/components/sections",
+          "@shared": "src/components/shared",
+          "@styled": "src/components/styled",
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
+  ],
+};
