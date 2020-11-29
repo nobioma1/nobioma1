162b99a00704df60c250c45e0dedc0901b2dbf64
Noble Obioma <nobioma1@gmail.com>
2020-11-29 07:31:32 +0100

    Add sections

--- force-awakens/src/pages/index.js
+++ force-awakens/src/pages/index.js
@@ -1,5 +1,24 @@
-import React from "react"
+import React from "react";
+
+import Layout from "@layouts/layout";
+import Header from "@components/header";
+import Hero from "@sections/hero";
+import Featured from "@sections/featured";
+import About from "@sections/about";
+import Projects from "@sections/projects";
+import Footer from "@sections/footer";
+import Articles from "@sections/articles";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <Header />
+      <Hero />
+      <Featured />
+      <About />
+      <Projects />
+      <Articles />
+      <Footer />
+    </Layout>
+  );
 }
