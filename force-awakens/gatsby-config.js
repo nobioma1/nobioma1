7b3528d9718075be665340e76e2f73c5077ce1d7
Noble Obioma <nobioma1@gmail.com>
2020-12-18 18:46:47 +0100

    feat: add react-helmet and site-metadata

--- force-awakens/gatsby-config.js
+++ force-awakens/gatsby-config.js
@@ -1,5 +1,11 @@
 module.exports = {
+  siteMetadata: {
+    title: `Noble Obioma`,
+    description: `A full-stack software engineer building high quality, scalable solutions for the web across a wide range of industries. My ultimate passion is building software and tackling new problems.`,
+    author: `@nobooln`,
+  },
   plugins: [
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-alias-imports`,
       options: {
@@ -24,6 +30,18 @@ module.exports = {
       resolve: `gatsby-plugin-styled-components`,
       options: {
         displayName: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#1c2541`,
+        theme_color: `#1c2541`,
+        display: `minimal-ui`,
+        icon: `src/images/avatar.png`,
       },
     },
     `gatsby-transformer-sharp`,
