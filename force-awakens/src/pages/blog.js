0dcce35e97849ba860268fce7b11408221be8b39
Noble Obioma <nobioma1@gmail.com>
2020-12-18 21:23:14 +0100

    add blog page

--- force-awakens/src/pages/blog.js
+++ force-awakens/src/pages/blog.js
@@ -1,13 +1,19 @@
 import React from "react";
+
+import Main from "@layouts/main";
+import MaxWidthLayout from "@layouts/maxWidth";
+import { StyledSectionsWrapper } from "@styled/sections";
 import SEO from "@components/seo";
 import data from "../data";
 
 const BlogPage = () => {
   return (
-    <div>
+    <Main>
       <SEO title="Blog" keywords={data.stack} />
-      my blog
-    </div>
+      <MaxWidthLayout>
+        <StyledSectionsWrapper>my blog</StyledSectionsWrapper>
+      </MaxWidthLayout>
+    </Main>
   );
 };
 
